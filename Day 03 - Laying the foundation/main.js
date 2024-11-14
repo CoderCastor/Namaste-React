a933efd2b719f3049fad8dc44367406a89341abf
codercastor <tejasshinde935@gmail.com>
2024-11-14 20:39:24 +0530

    learned styles for valid react fucntional components

--- Day 03 - Laying the foundation/main.js
+++ Day 03 - Laying the foundation/main.js
@@ -9,4 +9,21 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const heading = React.createElement("h1",{},"Hello I'am Working");
 
 root.render(heading); 
+ 
+//React Functional component
+// noraml writting 
+const headingComponent = () => {
+  return (
+    <h1>Namaste React</h1>
+  )
+}
 
+//writing valid style 1 - for single line
+const headingComponent1 = () => <h1>Namaste React</h1>
+
+//writing valid style 2 - for mult lines
+const headingComponent2 = () => (
+  <h1>
+    Namaste React
+  </h1>
+);
